342d86dd35857dd3fd99d8bf1b56990cf0452120
lnbalon <lnbalon87@gmail.com>
2020-12-20 23:51:06 +0800

    few edits

---
+++ baccarat.py
@@ -0,0 +1,31 @@
+import random
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import warnings
+
+warnings.filterwarnings('ignore')
+
+def shuffle_shoe(n_decks=8):
+    '''A shoe shuffling function'''
+
+    return pass
+
+
+def deal_game(shoe):
+
+    '''Deals a game from a shoe. Assumes that the shoe has enough card
+    to finish the game'''
+
+    return pass
+
+def baccarat_simulator(n_games):
+
+    '''A wrapper function that returns the result of each baccarat game for
+    n_games'''
+
+    return pass
+
+def 
